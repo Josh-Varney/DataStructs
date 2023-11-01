868dee4ee1099a134539a7c2637600885f93b7a8
Joshua Varney <up2138017@myport.ac.uk>
2023-11-01 23:47:51 +0000

    Update to methods insert and delete in LinkedList.py

--- linkedList.py
+++ linkedList.py
@@ -7,7 +7,32 @@ class LinkedList:
     def __init__(self):
         self.head = None
         
+    def appendNode(self, nodeVal):
+        if not self.head:
+            self.head = Node(nodeVal)
+        else:
+            currentNode = self.head
+            while currentNode.pointer:
+                currentNode = currentNode.pointer
+            currentNode.pointer = Node(nodeVal)
+
+    
+    def deleteNode(self, key):
+        if not self.head:
+            return None
+        else:
+            if self.head is key:
+                self.head.val = key
+            else:
+                currentNode = self.head
+                while currentNode.pointer:
+                    currentNode = currentNode.pointer
+                
+                    
+        
 if __name__ == '__main__':
-    node1 = Node(1)
-    node2 = Node(2)
-    node3 = Node(3)
+    linklist1 = LinkedList()
+    linklist1.appendNode(8)
+    linklist1.appendNode(7)
+    linklist1.appendNode(6)
+    linklist1.appendNode(9)
