84d12779a71ddaf1b826e54cb2a53893859e6e61
Joshua Varney <up2138017@myport.ac.uk>
2023-11-01 20:47:58 +0000

    updated a linked list class

--- linkedList.py
+++ linkedList.py
@@ -1,4 +1,13 @@
-class TreeNode:
+class Node:
     def __init__(self, val):
         self.val = val
         self.pointer = None
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        
+if __name__ == '__main__':
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
