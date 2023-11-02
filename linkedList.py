29bcc74de3dccaa0bd1aa5a05b48d707f252f45a
Joshua Varney <up2138017@myport.ac.uk>
2023-11-02 23:01:07 +0000

    Merge circular list

--- linkedList.py
+++ linkedList.py
@@ -51,8 +51,57 @@ class LinkedList:
             currentNode=currentNode.pointer
         return lString
                 
+class circularLinked(LinkedList):
+    def __init__(self):
+        super().__init__()
+        
+    def appendNode(self, nodeVal):
+        new_node=Node(nodeVal)
+        if not self.head:
+            self.head=new_node
+            self.head.pointer=self.head
+        currentNode=self.head
+        while currentNode.pointer!=self.head:
+            currentNode=currentNode.pointer
+        currentNode.pointer=new_node
+        new_node.pointer=self.head
+    
+    def deleteNode(self, key):
+        if not self.head:return
+        # Case: If head is only node
+        if self.head.val==key and self.head.pointer==self.head:
+            self.head=None 
+            return
+        # Case: Key in head node
+        if self.head.val==key:
+            currentNode=self.head
+            while currentNode.pointer!=self.head:
+                currentNode=currentNode.pointer
+            currentNode.pointer=self.head.pointer
+            self.head=self.head.pointer
+        else:
+            # Case: Key not in head node
+            currentNode=self.head
+            previousNode=None
+            while currentNode.pointer!=self.head and currentNode.val!=key:
+                previousNode=currentNode
+                currentNode=currentNode.pointer
                 
-                
+            if currentNode.val!=key:raise ValueError('Key Not In List') 
+            
+            previousNode.pointer=currentNode.pointer
+            
+    def __str__(self):
+        currentNode=self.head
+        cString=''
+        while True:
+            cString+=str(currentNode.val)+' '
+            currentNode=currentNode.pointer
+            if currentNode==self.head:
+                break
+        return cString
+            
+        
                 
 if __name__ == '__main__':
     linklist1 = LinkedList()
@@ -61,8 +110,21 @@ if __name__ == '__main__':
     linklist1.appendNode(6)
     linklist1.appendNode(9)
 
-    linklist1.insertNode(6,10)
-    print(linklist1)
+    # linklist1.insertNode(6,10)
+    # print(linklist1)
     
-    linklist1.deleteNode(9)
-    print(linklist1)
+    # linklist1.deleteNode(9)
+    # print(linklist1)
+    
+    clinklist1 = circularLinked()
+    
+    clist = [1,2,3,4,5,6,7,8]
+    for num in clist:
+        clinklist1.appendNode(num)
+    print(clinklist1)
+    
+    clinklist1.appendNode(9)
+    print(clinklist1)
+    
+    clinklist1.deleteNode(1)
+    print(clinklist1)
