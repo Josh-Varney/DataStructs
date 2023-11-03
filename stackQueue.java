5308295463595a680dafa4415c7e801e8b0208cb
Joshua Varney <up2138017@myport.ac.uk>
2023-11-03 13:25:51 +0000

    Adding contents to stackQueue.java

--- stackQueue.java
+++ stackQueue.java
@@ -1,3 +1,17 @@
 public class stackQueue {
-    
+    private Node head;
+    private Node tailNode;
+    public static void main(String[] args) {
+        
+    }
+}
+
+class SNode {
+    int val;
+    int content;  //Temp may change
+
+    public SNode(int val, int content){
+        this.val = val;
+        this.content = content;
+    }
 }
