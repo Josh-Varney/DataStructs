a7d0d96faf4b34f81fbe57d0891fd4f06b811ea6
Joshua Varney <up2138017@myport.ac.uk>
2023-11-04 13:09:29 +0000

    -a

---
+++ Java DS/queue.java
@@ -0,0 +1,15 @@
+
+
+public class queue extends stack {
+    public static void main(String[] args) {
+        queue queue1 = new queue();
+        
+    }
+
+    
+    public queue(){
+        super();
+    }
+    
+}
+
