060de2d4de865a5383477cc55f22f6ad6b0a2a89
Joshua Varney <up2138017@myport.ac.uk>
2023-11-05 10:10:14 +0000

    --all

--- Java DS/trees.java
+++ Java DS/trees.java
@@ -1,12 +1,15 @@
-/**
- * trees
- */
 public class trees {
     TreeNode root;
 
     public static void main(String[] args) {
         trees btree = new trees();
         btree.appendNode(0);
+        // Add more nodes if needed
+        btree.appendNode(1);
+        btree.appendNode(-1);
+        // Print the tree
+        btree.removeNode(btree.root, 1);
+        btree.printTree(btree.root);
     }
 
     public trees(){
@@ -14,31 +17,79 @@ public class trees {
     }
 
     public void appendNode(int val){
+        // No Node
         if (root == null){
             root = new TreeNode(val);
             return;
         }
 
-        TreeNode currentNode = root;
-        while (true){
-            if (currentNode.val < val){
-                if (currentNode.right == null){
-                    currentNode.right = new TreeNode(val);
-                    return;
-                } else {
-                    currentNode = currentNode.left;
-                }
-            } else{
-                if (currentNode.left == null){
-                    currentNode.left = new TreeNode(val);
-                    return;
-                } else {
-                    currentNode = currentNode.right;
-                }
+        insertNode(root, val);
+    }
+
+    private void insertNode(TreeNode currentNode, int val) {
+        // Key is less than currentVal
+        if (val < currentNode.val) {
+            if (currentNode.left == null) {
+                currentNode.left = new TreeNode(val);
+            } else {
+                insertNode(currentNode.left, val);
+            }
+        // Key is greater than or equal to currentVal
+        } else {
+            if (currentNode.right == null) {
+                currentNode.right = new TreeNode(val);
+            } else {
+                insertNode(currentNode.right, val);
             }
         }
     }
-    
+
+    private TreeNode removeNode(TreeNode node, int val) {
+        // Error Checking
+        if (node == null){
+            throw new IllegalArgumentException("Empty Tree");
+        }
+        // Key is less than
+        if (val < node.val){
+            node.left = removeNode(node.left, val);
+        // Key is greater than
+        } else if (val > node.val){
+            node.right = removeNode(node.right, val);
+        } else{
+            // Node with only one child or no children
+            if (node.left == null){
+                return node.right;
+            } else if (node.right == null){
+                return node.left;
+            }
+            
+            // Node with two children
+            node.val = minValue(node.right);
+            // Delete the in-order successors
+            node.right = removeNode(node.right, node.val);
+        }
+
+        return node;
+        
+    }
+
+    private int minValue(TreeNode node){
+        int minValue = node.val;
+        while (node.left != null){
+            minValue = node.left.val;
+            node = node.left;
+        }
+        return minValue;
+    }
+
+    public void printTree(TreeNode node) {
+        // in-order traversal
+        if (node != null) {
+            printTree(node.left);
+            System.out.print(node.val + " ");
+            printTree(node.right);
+        }
+    }
 }
 
 class TreeNode {
